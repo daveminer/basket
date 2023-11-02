2a69ac3e6a7ec86ed80e1d4907bf1cbdb1339681
daveminer <wtrispsn@gmail.com>
2023-11-01 22:44:37 -0400

    ws logger infos

--- lib/basket/alpaca/ws_client.ex
+++ lib/basket/alpaca/ws_client.ex
@@ -2,68 +2,46 @@ defmodule Basket.Alpaca.WsClient do
   use WebSockex
   require Logger
 
+  @default_subscription_msg "{\"action\":\"subscribe\",\"trades\":[\"AAPL\"],\"quotes\":[\"AMD\",\"CLDR\"],\
+          \"bars\":[\"*\"]}"
   def start_link(state) do
-    IO.puts("Starting WS Client: #{iex_feed()}")
+    Logger.info("Starting Alpaca websocket client.")
 
-    auth_headers = [
-      {"APCA-API-KEY-ID", api_key()},
-      {"APCA-API-SECRET-KEY", api_secret()}
-    ]
-
-    # WebSockex.start_link(iex_feed(), __MODULE__, state, extra_headers: auth_headers)
-    WebSockex.start_link(iex_feed(), __MODULE__, state)
+    WebSockex.start_link(iex_feed(), __MODULE__, state, extra_headers: auth_headers())
   end
 
-  def handle_connect(conn, state) do
-    Logger.info("Connected! #{inspect(conn)} ::: #{state}")
+  def handle_connect(_conn, state) do
+    Logger.info("Alpaca websocket connected.")
     {:ok, state}
   end
 
   def handle_disconnect(disconnect_map, state) do
-    Logger.info("Local close")
-    # Logger.info("Local close with reason: #{inspect(disconnect_map)}")
+    Logger.info("Alpaca websocket disconnected.")
     super(disconnect_map, state)
-  end
-
-  def handle_cast({:send, {type, msg} = frame}, state) do
-    IO.puts("Cast state: #{state}")
-    IO.puts("Sending #{type} frame with payload: #{msg}")
-    {:reply, frame, state}
   end
 
   @doc """
   Receive a connection success message and respond with an authentication attempt.
   """
-  def handle_frame({type, "[{\"T\":\"success\",\"msg\":\"connected\"}]"}, state) do
+  def handle_frame({_type, "[{\"T\":\"success\",\"msg\":\"connected\"}]"}, state) do
     Logger.info("Connection Message Received.")
 
-    # Logger.info("API KEY: #{api_key()}")
-    # Logger.info("API SECRET: #{api_secret()}")
-
-    msg = Jason.encode!(%{"action" => "auth", "key" => api_key(), "secret" => api_secret()})
-    IO.inspect("MSG: #{msg}")
-    # IO.inspect("REsult: #{result}, Sending: #{msg}")
-    # msg = "{\"action\":\"subscribe\",\"trades\":[\"AAPL\"],\"quotes\":[\"AMD\",\"CLDR\"],\
-    #       \"bars\":[\"*\"]}"
-    {:reply, {type, msg}, state}
-    # {:ok, state}
+    {:ok, state}
   end
 
   def handle_frame({type, "[{\"T\":\"success\",\"msg\":\"authenticated\"}]"}, state) do
-    Logger.info("Authenticated!")
+    Logger.info("Alpaca websocket authenticated.")
 
-    msg = "{\"action\":\"subscribe\",\"trades\":[\"AAPL\"],\"quotes\":[\"AMD\",\"CLDR\"],\
-      \"bars\":[\"*\"]}"
-    {:reply, {type, msg}, state}
-    # {:ok, state}
+    {:reply, {type, @default_subscription_msg}, state}
   end
 
   def handle_frame({_type, msg}, state) do
     Logger.info("Received Message: #{msg}")
 
-    Logger.info("State: #{state}")
-    {:reply, {:text, msg}, :fake_state}
+    {:ok, state}
   end
+
+  defp auth_headers, do: [{"APCA-API-KEY-ID", api_key()}, {"APCA-API-SECRET-KEY", api_secret()}]
 
   defp iex_feed, do: "#{ws_server_url()}/iex"
 

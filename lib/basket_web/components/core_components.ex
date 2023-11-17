c8c9f2b64b657bc20562bc0e5566ba006719785c
daveminer <wtrispsn@gmail.com>
2023-11-16 20:33:44 -0500

    add hover shading to ticker table

--- lib/basket_web/components/core_components.ex
+++ lib/basket_web/components/core_components.ex
@@ -268,7 +268,7 @@ defmodule BasketWeb.CoreComponents do
     <button
       type={@type}
       class={[
-        "phx-submit-loading:opacity-75 rounded-lg bg-zinc-900 hover:bg-zinc-700 py-2 px-3",
+        "phx-submit-loading:opacity-75 rounded-lg py-2 px-3",
         "text-sm font-semibold leading-6 text-white active:text-white/80",
         @class
       ]}

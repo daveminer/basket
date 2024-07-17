2543e78e761379e91043ab947e024a50c33d40a5
daveminer <wtrispsn@gmail.com>
2024-07-17 00:14:54 -0400

    remove mailer config

--- lib/basket/mailer.ex
+++ lib/basket/mailer.ex
@@ -1,41 +1,5 @@
 defmodule Basket.Mailer do
   @moduledoc false
 
-  use Pow.Phoenix.Mailer
   use Swoosh.Mailer, otp_app: :basket
-
-  import Swoosh.Email
-
-  require Logger
-
-  @impl true
-  def cast(%{user: user, subject: subject, text: text, html: html}) do
-    %Swoosh.Email{}
-    |> to({"", user.email})
-    |> from({"Basket", "no-reply@halyard.systems"})
-    |> subject(subject)
-    |> html_body(html)
-    |> text_body(text)
-  end
-
-  @impl true
-  def process(email) do
-    # An asynchronous process should be used here to prevent enumeration
-    # attacks. Synchronous e-mail delivery can reveal whether a user already
-    # exists in the system or not.
-
-    Task.start(fn ->
-      email
-      |> deliver()
-      |> log_warnings()
-    end)
-
-    :ok
-  end
-
-  defp log_warnings({:error, reason}) do
-    Logger.warning("Mailer backend failed with: #{inspect(reason)}")
-  end
-
-  defp log_warnings({:ok, response}), do: {:ok, response}
 end

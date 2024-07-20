1affc19dbd3d75786114d9e7bc389e305c46f8e0
daveminer <wtrispsn@gmail.com>
2024-07-20 01:21:45 -0400

    use prod alpaca websocket endpoint in dev

--- config/dev.exs
+++ config/dev.exs
@@ -33,8 +33,8 @@ config :basket, :alpaca,
   api_key: System.get_env("ALPACA_API_KEY"),
   api_secret: System.get_env("ALPACA_API_SECRET"),
   data_http_url: "https://data.alpaca.markets",
-  market_http_url: "https://api.alpaca.markets/v2",
-  market_ws_url: "wss://stream.data.alpaca.markets/v2/test"
+  market_http_url: "https://api.alpaca.markets",
+  market_ws_url: "wss://stream.data.alpaca.markets/v2/iex"
 
 # ## SSL Support
 #

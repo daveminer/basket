d199316f81912c7cbae6e044e895590dc90895e0
Dave Miner <wtrispsn@gmail.com>
2024-10-23 00:06:41 -0400

    preload offices for setting page context

--- lib/basket_web/controllers/settings/settings_controller.ex
+++ lib/basket_web/controllers/settings/settings_controller.ex
@@ -1,7 +1,17 @@
 defmodule BasketWeb.SettingsController do
   use BasketWeb, :controller
 
+  alias Basket.Repo
+
   def index(conn, _params) do
+    conn =
+      if user = Pow.Plug.current_user(conn) do
+        user = Repo.preload(user, :offices)
+        assign(conn, :current_user, user)
+      else
+        conn
+      end
+
     render(assign(conn, :form, %{}), "index.html")
   end
 end

563c4e80424bad99a2b564cff74646a28cfcd578
Dave Miner <wtrispsn@gmail.com>
2025-05-15 01:04:53 -0400

    lint

--- config/runtime.exs
+++ config/runtime.exs
@@ -31,7 +31,8 @@ config :basket, :news,
   sentiment_service_enabled: sentiment_service_enabled,
   sentiment_service_url: System.get_env("SENTIMENT_SERVICE_URL") || "http://localhost:8000",
   # Set this if calling back to a local server
-  sentiment_callback_url: System.get_env("SENTIMENT_CALLBACK_URL") || Application.fetch_env!(:basket, :host)
+  sentiment_callback_url:
+    System.get_env("SENTIMENT_CALLBACK_URL") || Application.fetch_env!(:basket, :host)
 
 if config_env() == :prod do
   database_url =
